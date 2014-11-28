ec22c7c989bbd7875487c14518e44272b0664b95
Kevin Alexander Höfle <kevinahoefle@gmail.com>
2014-11-28 11:21:33 +0100

    Update lab1.asm
    first working

--- lab1.asm
+++ lab1.asm
@@ -1,7 +1,7 @@
 ;Dateinamen:
 ;Beschreibung:
 ;VERSION 0.2
-; 
+;
 ;INVERTER DOES NOT WORK PROPERLY ATM FOR THE BUZZER
 ;
 ;Entwickler: Khanh Nguyen, Kevin Höfle
@@ -31,34 +31,25 @@ IntVectLow
 ;//No Interrupts Enabled
 org 0x30
 ;//Initialisation Code - Placed at some point after IntVect
-
-
 ;Variablen
-
-; BANK 0 -> Zeitschleifen, die öfters aufgerufen werden,  am besten hier rein  in diese Bank da diese am schnellsten zugegriffen werden
+; BANK 0 -> Zeitschleifen, die öfters aufgerufen werden, am besten hier rein in diese Bank da diese am schnellsten zugegriffen werden
 var1 equ 0x01 ;über var1 kann dann die zeit eingestellt werden
-
-
-
 Init
 ;Eingägen -> 1
 ;Ausägnge -> 0
 
-clrf PORTA
+clrf PORTA	
 movlw 0xFF ;besser alles auf Eingänge
 movwf TRISA, 0
-
-
 ;LED-Ports //ANALOG zur A
+
 clrf PORTB
 movlw 0xf0
 movwf TRISB, 0
 
-
 clrf PORTC
 movlw 0xfb ; ALLE BIS AUF den einen ausgang auf eingange setzen, auch wenn diese nicht gebraucht werden, da es immer besser ist diese als Eingang zu benutzen (kein strom fließt raus!)
 movwf TRISC, 0
-
 
 ;AD-WANDLER EINSTELLUNGEN
 ;ADCON0 10 (fosc32) 000 (An0 soll gewandelt werden) 0 (Ad-wandler not in progress) 0 (undefined) 1 (Ad-Wandler anschalten) -> 1 00 000 0 0 1 -> 0x81
@@ -66,64 +57,52 @@ movwf TRISC, 0
 ;1 0 0 0 1 1 1 0
 movlw 0x81
 movwf ADCON0, 0
-
-movlw 0x8E
+movlw 0x0E
 movwf ADCON1, 0
-
 bra main
 ;//Main Application Code
 main
 
 
 tester
-    CLRWDT
-    BTFSS PORTA, 4
-    BRA tester ;springt jedes mal wieder zurück wenn Bit4 nicht gesetzt ist
+BTFSC PORTA, 4
+BRA tester ;springt jedes mal wieder zurück wenn Bit4 nicht gesetzt ist
 
 
 ;Hier muss der AD-Wandler jetzt starten -> ALSO got bit setzen!
-
-
 BSF ADCON0, 2, 0
-
 ;DATEN werden jetzt in ADRESSH geschrieben und müssen an die Portsweitergeleitet werden
-
 ;MOVF ADRESSH, 0, 0 ;von ADRESSH ins wreg kann weg gelassen werden dank swapf befehl
-SWAPF ADRESH, 1,0 ;nibbles tauschen KANN MIT SWAPF DIREKT INS WREG GESCHRIEBEN WERDEN?! ALSO MOVF VORHER RAUSNEHMEN!
-MOVFF WREG, PORTA ;; werte an die LED schicken
-
 
 ;;DIE SCHLEIFE SOLL ZEITABHÄNGIG VOM EINGESTELLTEN WERT IN ADRESSH (oder Wreg aber da is es schon getauscht)
 ;;den Summer invertieren
 ;;vorher muss geschaut werden ob der AD-Wandler fertig ist
 
+
 wait_for_ad
-    BTFSS ADCON0, 2 ;chekcen ob auf 0
+BTFSC ADCON0, 2 ;chekcen ob auf 0
 BRA wait_for_ad
-
-SWAPF WREG, 1, 0 ;;WREG wieder rum drehen
-
-
- BTG PORTC, 2 
+;SWAPF WREG, 1, 0 ;;WREG wieder rum drehen
 
 
+SWAPF ADRESH, 0,0 ;nibbles tauschen k
 
-call inverter
+MOVWF PORTB, 0 ;; werte an die LED schicken
+SWAPF WREG, 1,0 
+BTG PORTC, RC2
 
-BTG PORTC, 2
-;;könnte auch als subroutine verwendet werden
+
+
+;call inverter
+
 inverter
-    DECFSZ WREG, 0 ;;bis wreg 0 is 
+DECFSZ WREG, 0 ;;bis wreg 0 is
 bra inverter
-    
 
-
+BTG PORTC, RC2
 
 
 bra main
-
-
-
 FinishLoop
 bra FinishLoop ;//Do.. Forever stops
 end
